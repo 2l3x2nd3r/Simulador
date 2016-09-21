76c7eacf04b227fd77fa73f02062e8e4020de364
Cedu 3 Cuenta Generica) <cedu3@uninorte.local>
2016-09-21 17:47:11 -0500

    grafico

--- js/app.js
+++ js/app.js
@@ -32,3 +32,36 @@ angular.module("simu",[])
 		o10:0
 	}
 }]);
+var lineChartData = {
+	labels : ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio"],
+	datasets : [
+		{
+			label: "Primera serie de datos",
+			fillColor : "rgba(220,220,220,0.2)",
+			strokeColor : "#6b9dfa",
+			pointColor : "#1e45d7",
+			pointStrokeColor : "#fff",
+			pointHighlightFill : "#fff",
+			pointHighlightStroke : "rgba(220,220,220,1)",
+			data : [10,30,40,45,40,30,50,80]
+		}
+		// {
+		// 	label: "Segunda serie de datos",
+		// 	fillColor : "rgba(151,187,205,0.2)",
+		// 	strokeColor : "#e9e225",
+		// 	pointColor : "#faab12",
+		// 	pointStrokeColor : "#fff",
+		// 	pointHighlightFill : "#fff",
+		// 	pointHighlightStroke : "rgba(151,187,205,1)",
+		// 	data : [40,50,70,40,85,55,15]
+		// }
+	]
+}
+var ctx4 = document.getElementById("chart-area4").getContext("2d");
+var canva = new Chart(ctx4).Line(lineChartData, {responsive:true});
+document.getElementById("chart-area4").onclick = function(evt){
+ var activePoints = canva.getPointsAtEvent(evt);
+ var firstPoint = activePoints[0];
+ if (firstPoint !== undefined)
+		 alert(firstPoint.value);
+};
